e38ef2a1c709254cc5737dba9683eee95a684843
denkrasn <den.job4j@yandex.ru>
2021-09-30 09:53:36 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/test/java/ru/job4j/tracker/oop/BallStory.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.oop;
+
+public class BallStory {
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Hare hare = new Hare();
+        Wolf wolf = new Wolf();
+        Fox fox = new Fox();
+
+        hare.tryEat(ball);
+        ball.tryRun(false);
+        wolf.tryEat(ball);
+        ball.tryRun(false);
+        fox.tryEat(ball);
+        ball.tryRun(true);
+
+    }
+}
