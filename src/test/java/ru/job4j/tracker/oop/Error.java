a4ef7d03c0e306b6947685027c19fdf13345d35f
denkrasn <den.job4j@yandex.ru>
2021-10-01 10:03:04 +0300

    1.7. Конструктор [#173889]

---
+++ src/test/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error arithmeticException = new Error();
+        arithmeticException.printInfo();
+        Error arrayIndexOutOfBoundsException = new Error(true, 1243, " ArrayIndexOutOfBoundsException");
+        arrayIndexOutOfBoundsException.printInfo();
+        Error classCastException = new Error(true, 2042, "ClassCastException");
+        classCastException.printInfo();
+    }
+}
