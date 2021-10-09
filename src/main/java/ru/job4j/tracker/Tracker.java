3a936e3e36d5ee8f0d03610769cae38bcb18d274
denkrasn <den.job4j@yandex.ru>
2021-10-09 11:09:26 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,19 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[items.length];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item itm = items[index];
+            if (itm != null) {
+                itemsWithoutNull[count] = itm;
+                count++;
+            }
+        }
+        return Arrays.copyOf(itemsWithoutNull, count);
     }
 
     public Item findById(int id) {
@@ -21,5 +36,18 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] findName = new Item[items.length];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item itm = items[index];
+            if (itm.getName().equals(key)) {
+                findName[count] = itm;
+                count++;
+            }
+        }
+        return Arrays.copyOf(findName, count);
     }
 }
