396152c039feb45dd33743cb6268f44de32ce42d
denkrasn <den.job4j@yandex.ru>
2022-04-15 16:27:33 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("deniskras@gmail.com", "Denis Krasnoshtan");
+        map.put("deniskras@yandex.ru", "Denis Krasnoshtan");
+        map.put("deniskras@rambler.ru", "Denis Krasnoshtan");
+        for (Map.Entry<String, String> entry: map.entrySet()){
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " = " + value);
+        }
+    }
+}
