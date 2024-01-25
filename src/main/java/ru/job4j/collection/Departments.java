5e477ae873a574bec4a11bb143e7835ae5156f55
DenisKrasnoshtan <kra5den@yandex.ru>
2024-01-25 19:52:30 +0300

    3. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -3,7 +3,6 @@ package ru.job4j.collection;
 import java.util.*;
 
 public class Departments {
-
     public static List<String> fillGaps(List<String> departments) {
         Set<String> temp = new LinkedHashSet<>();
         for (String value : departments) {
@@ -15,7 +14,6 @@ public class Departments {
         }
         return new ArrayList<>(temp);
     }
-
 
     public static void sortAsc(List<String> orgs) {
         orgs.sort(Comparator.naturalOrder());
