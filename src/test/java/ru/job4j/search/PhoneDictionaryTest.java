268bf72ea9fef65875df18539bcd134390f3d560
DenisKrasnoshtan <kra5den@yandex.ru>
2024-02-10 14:27:15 +0300

    2. Local-Variable Type Inference [#70626]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -7,17 +7,6 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class PhoneDictionaryTest {
-
-    @Test
-    public void whenFindByName() {
-        var phones = new PhoneDictionary();
-        phones.add(
-                new Person("Petr", "Arsentev", "534872", "Bryansk")
-        );
-        var persons = phones.find("Petr");
-        assertThat(persons.get(0).getSurname(), is("Arsentev"));
-    }
-
     @Test
     public void whenNotFindByName() {
         var phones = new PhoneDictionary();
